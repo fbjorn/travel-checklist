be9adc48e52f0c98bc05413c9813613e21b3e8f4
fbjorn <denis@fbjorn.cc>
2025-05-14 00:59:05 +0300

    Tiny update

--- src/routes/checklists/+page.svelte
+++ src/routes/checklists/+page.svelte
@@ -2,20 +2,33 @@
 	import { listSavedChecklists } from '$lib/storage';
 	import type { SavedChecklist } from '$lib/types';
 	import { onMount } from 'svelte';
-
+	import { Card, CardHeader, CardTitle, CardContent, CardDescription } from '$lib/components/ui/card';
+	import { formatDate } from '$lib/utils';
 	let checklists = $state<SavedChecklist[]>([]);
 
 	onMount(() => {
 		checklists = listSavedChecklists();
 	});
+
+    function calcProgress(checklist: SavedChecklist) {
+        const totalItems = checklist.sections.reduce((acc, section) => acc + section.items.length, 0);
+        const checkedItems = checklist.sections.reduce((acc, section) => acc + section.items.filter((item) => item.checked).length, 0);
+        return Math.round((checkedItems / totalItems) * 100);
+    }
 </script>
 
-<div class="mt-8">
-	<h1>Checklists</h1>
-
+<div class="mt-8 flex flex-col gap-4">
 	{#each checklists as checklist}
-		<div class="flex justify-between items-center">
-			<a href={`/checklists/${checklist.id}`}>{checklist.name}</a>
-		</div>
+		<a href={`/checklists/${checklist.id}`}>
+			<Card>
+				<CardHeader>
+					<CardTitle>{checklist.name}</CardTitle>
+                    <CardDescription>Packed {calcProgress(checklist)}% of items</CardDescription>
+				</CardHeader>
+				<CardContent class="-mt-4">
+					<p class="text-sm text-gray-500">Created on {formatDate(checklist.date)}</p>
+				</CardContent>
+			</Card>
+		</a>
 	{/each}
 </div>
